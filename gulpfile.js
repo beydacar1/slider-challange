e3ab8585ed5416d6458c508f5310336585f3e2f5
Beyda <beydacar1@gmaill.com>
2020-07-16 01:47:33 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require("gulp-watch");
+
+gulp.task('sass', function () {
+    return gulp.src('./scss/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./css'));
+  });
+
+  gulp.task("watch", function () {
+    gulp.watch("./scss/*.scss", gulp.series("sass"));
+  });
+
+  gulp.task("default", gulp.series("sass", "watch"));
